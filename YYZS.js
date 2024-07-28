9f671ebbc4353f23bd9f0b7eacefa428cab52511
betraykk <166674004+betraykk@users.noreply.github.com>
2024-07-28 14:43:30 +0800

    Update YYZS.js

--- YYZS.js
+++ YYZS.js
@@ -4,9 +4,6 @@
 特别说明：CK且用且珍惜,全功能解锁
 免责声明：仅供学习 请勿传播，请勿在闲鱼售卖
 更新时间: 2023.11.05.17.19
-[rewrite_local]
-^https://newdrugs.dxy.cn url script-request-header https://raw.githubusercontent.com/betraykk/Quantumult-x/main/YYZS.js
-https://newdrugs.dxy.cn/app/user/pro/stat? url script-response-body https://raw.githubusercontent.com/betraykk/Quantumult-x/main/YYZS.js
 
 [mitm]
 hostname = newdrugs.dxy.cn
