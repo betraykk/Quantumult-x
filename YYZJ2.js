32db8a43dee5303cc0238229761126c27d672d27
betraykk <166674004+betraykk@users.noreply.github.com>
2024-07-28 12:24:40 +0800

    更新 YYZJ2.js

--- YYZJ2.js
+++ YYZJ2.js
@@ -5,6 +5,7 @@
 免责声明：仅供学习 请勿传播，请勿在闲鱼售卖
 更新时间: 2023.11.05.17.19
 [rewrite_local]
+^https://newdrugs.dxy.cn url script-request-header https://github.com/betraykk/Quantumult-x/blob/main/YYZJ2.js
 https://newdrugs.dxy.cn/app/user/pro/stat? url script-response-body https://github.com/betraykk/Quantumult-x/blob/main/YYZJ2.js
 [mitm]
 hostname = newdrugs.dxy.cn
