a59f558468e63b904d90b9cdafdbf522db088f1a
betraykk <166674004+betraykk@users.noreply.github.com>
2025-05-28 22:18:14 +0800

    更新 xiaohei.js

--- xiaohei.js
+++ xiaohei.js
@@ -2,6 +2,6 @@
 
 
 # > 台铃智能
-xiaoheianzuosmart.applink.smart321.com
+hostname = xiaoheianzuosmart.applink.smart321.com
 # 开屏广告
 ^https://www\.xiaoheianzuosmart.applink.smart321\.com/v8/smart/app/config/get$ url response-body "data":\{.+\} response-body "data":{}
